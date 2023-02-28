e8f0fc4eedaf0358f533e27074b7c7436432ee95
jerobejrobinson <jerobejrobinson@gmail.com>
2023-02-28 10:06:29 -0600

    first commit

---
+++ app/root.jsx
@@ -0,0 +1,70 @@
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useLoaderData,
+} from '@remix-run/react';
+// import styles from './styles/app.css';
+import tailwind from './styles/tailwind-build.css';
+import favicon from '../public/favicon.svg';
+import {Layout} from './components/Layout';
+
+export const links = () => {
+  return [
+    {rel: 'stylesheet', href: tailwind},
+    {
+      rel: 'preconnect',
+      href: 'https://cdn.shopify.com',
+    },
+    {
+      rel: 'preconnect',
+      href: 'https://shop.app',
+    },
+    {rel: 'icon', type: 'image/svg+xml', href: favicon},
+  ];
+};
+
+export const meta = () => ({
+  charset: 'utf-8',
+  viewport: 'width=device-width,initial-scale=1',
+});
+
+export async function loader({context}) {
+  const layout = await context.storefront.query(LAYOUT_QUERY);
+  return {layout};
+}
+
+export default function App() {
+  const data = useLoaderData();
+
+  const {name} = data.layout.shop;
+
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        {/* <h1>Hello, {name}</h1>
+        <p>This is a custom storefront powered by Hydrogen</p> */}
+        <Layout title={name}>
+          <Outlet />
+        </Layout>
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
+
+const LAYOUT_QUERY = `#graphql
+  query layout {
+    shop {
+      name
+      description
+    }
+  }
+`;
